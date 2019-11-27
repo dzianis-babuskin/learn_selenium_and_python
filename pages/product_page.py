6f0a046c5004989547f2eb86c31f9ed2b280d169
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-11-27 17:05:07 +0300

    add methods to product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,11 +2,20 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
 
+
 class ProductPage(BasePage):
 
     def get_product_to_the_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
-
     
     def should_be_add_to_basket_button(self):
-        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to busket button is not present"
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), \
+            "Add to busket button is not present"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message should disappear, but it doesn't"
