f2a48b7d02bc7b136fb101d14ff667903076ea3d
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-11-27 17:31:00 +0300

    add test to product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 import pytest
 
+@pytest.mark.skip
 @pytest.mark.parametrize('number', [0, 1, 2, 3, 4, pytest.param(5, marks=pytest.mark.xfail), 6, 7, 8, 9])
 def test_guest_can_add_product_to_basket(browser, number):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
@@ -12,4 +13,30 @@ def test_guest_can_add_product_to_basket(browser, number):
     page.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
     page.is_element_present(*ProductPageLocators.BUSKET_TOTAL)
 
-    
+
+@pytest.mark.xfail    
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.get_product_to_the_basket()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "success message is displayed after adding product to basket"
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "success message is displayed after visiting page"
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.get_product_to_the_basket()
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "success message was not disappear after adding product to basket"
