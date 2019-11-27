0154be7ff25dd083368a69a76d2c3488f0e5c30c
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-11-27 17:03:59 +0300

    add parametrization to product page tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,15 @@
 from .pages.product_page import ProductPage
-from .pages import locators
+from .pages.locators import ProductPageLocators
+import pytest
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('number', [0, 1, 2, 3, 4, pytest.param(5, marks=pytest.mark.xfail), 6, 7, 8, 9])
+def test_guest_can_add_product_to_basket(browser, number):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
     page = ProductPage(browser, link)
     page.open()
     page.get_product_to_the_basket()
     page.solve_quiz_and_get_code()
+    page.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+    page.is_element_present(*ProductPageLocators.BUSKET_TOTAL)
 
     
