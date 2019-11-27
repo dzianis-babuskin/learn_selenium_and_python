960e9526f8f2a5921303bb5892191f73b8a1fbae
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-11-27 13:59:52 +0300

    add test product test module

---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+from .pages.product_page import ProductPage
+from .pages import locators
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.get_product_to_the_basket()
+    page.solve_quiz_and_get_code()
+
+    
