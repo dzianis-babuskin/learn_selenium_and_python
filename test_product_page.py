f52809b289bc1b4e867b0306164877ff86a4ef7c
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-11-28 15:45:47 +0300

    add test to product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
+from .pages.locators import BasePageLocators
 import pytest
 
 @pytest.mark.skip
@@ -40,3 +41,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.get_product_to_the_basket()
     assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
         "success message was not disappear after adding product to basket"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
