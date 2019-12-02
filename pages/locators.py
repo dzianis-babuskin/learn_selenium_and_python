5aa053d2b77d0afebc659a97a8d04ebaa733613a
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-12-02 14:18:58 +0300

    add basket page locators

--- pages/locators.py
+++ pages/locators.py
@@ -3,13 +3,20 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_LINK = (By.PARTIAL_LINK_TEXT, "basket")
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class BasketPageLocators(BasePageLocators):
+    CONTENT_INNER_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+    CONTENT_INNER = (By.CSS_SELECTOR, "#content_inner")
+    BASKET_SUMMARY = (By.CSS_SELECTOR, ".basket_summary")
 
 
-class LoginPageLocators():
+class MainPageLocators(BasePageLocators):
+    pass
+
+
+class LoginPageLocators(BasePageLocators):
     REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
@@ -18,7 +25,7 @@ class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class ProductPageLocators():
+class ProductPageLocators(BasePageLocators):
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
     PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".price_color")
