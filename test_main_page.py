290b4935daf297c42d5305fa112d9e21abc72a32
Dzianis Babuskin <dzianis_babuskin@epam.com>
2019-12-02 14:19:49 +0300

    add tests with no products in basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,14 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.locators import BasePageLocators
+from .pages.locators import BasketPageLocators
+from .pages.locators import ProductPageLocators
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+import pytest
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
@@ -11,8 +18,27 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    assert page.no_orders_in_basket
+    assert page.empty_is_present_in_message
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    assert page.no_orders_in_basket
+    assert page.empty_is_present_in_message
